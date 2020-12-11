b2ac7eae0590faceca8f10519a649d070c338c88
romanenkor <romu4444@gmail.com>
2020-12-11 20:59:18 +0300

    Added windows telemetry file

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ curl https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/Spyware
 curl https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SpywareFilter/sections/tracking_servers_firstparty.txt 2>/dev/null | egrep '^[|]' | cut -c 3- | sed 's/.$//' | grep -v '\$' | tee -a $FILE
 ) 1>/dev/null
 sed -i 's/^/0.0.0.0 /' $FILE
+
+FILE=windows-telemetry
+(
+curl https://gist.githubusercontent.com/tildebyte/b70ea62e38832197c07ac046899f25c8/raw/703edc9e4c9453ff066622623dca372406c8bced/ms_tracking_domains_blocklist.txt 2>/dev/null | tee -a $FILE
+) 1>/dev/null
+sed -i 's/^/0.0.0.0 /' $FILE
